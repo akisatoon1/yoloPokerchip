30097ac834d86bc8c05d8d2cf522762e8f46c613
akisatoon <akisatoon@gmail.com>
2026-08-21 22:59:46 +0900

    update: setup
    システムのtorchを使うため

--- setup_runpod.sh
+++ setup_runpod.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-python -m venv .venv
+python -m venv .venv --system-site-packages
 source .venv/bin/activate
 
-pip install uv
-uv pip install -r requirements.txt
+pip install -U pip
+pip install -r requirements.txt
