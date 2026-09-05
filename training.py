f6a68ad613b718d5e8ff3707ef054c930d188d73
Akisatoon <akisatoon@gmail.com>
2026-09-05 07:48:48 +0000

    fix: version_n = 3 -> 4

--- training.py
+++ training.py
@@ -133,7 +133,7 @@ def main():
         pretrained_dataset, epochs=EPOCHS, patience=PACIENCE, fraction=FRACTION
     )
     test_dataset = download_dataset(
-        ROBOFLOW_API_KEY, ROBOFLOW_WORKSPACE, ROBOFLOW_TEST_PROJECT, version_n=3
+        ROBOFLOW_API_KEY, ROBOFLOW_WORKSPACE, ROBOFLOW_TEST_PROJECT, version_n=4
     )
 
     global BEST_MODEL
